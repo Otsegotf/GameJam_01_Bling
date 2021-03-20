744e6a410a57649ca79becce87167f939e7b07c4
Lernom <sefghost@gmail.com>
2021-03-20 20:53:49 +0300

    game

--- Assets/Scripts/Game/BuyListManager.cs
+++ Assets/Scripts/Game/BuyListManager.cs
@@ -7,26 +7,26 @@ namespace GJgame
     public class BuyListManager : Singleton<BuyListManager>
     {
 
-        public Dictionary<ShopItem, int> CurrentList;
+        public Dictionary<string, int> CurrentList;
         public void GenerateList(int length)
         {
-            CurrentList = new Dictionary<ShopItem, int>();
+            CurrentList = new Dictionary<string, int>();
             AisleConstructor aisle = null;
             for (int i = 0; i < length; i++)
             {
                 while(aisle == null || !aisle.ItemPrefab)
                     aisle = GameManager.Instance.GetRandomAisle();
-                if(!CurrentList.ContainsKey(aisle.ItemPrefab))
+                if(!CurrentList.ContainsKey(aisle.ItemPrefab.name))
                 {
-                    CurrentList[aisle.ItemPrefab] = 0;
+                    CurrentList[aisle.ItemPrefab.name] = 0;
                 }
-                CurrentList[aisle.ItemPrefab]++;
+                CurrentList[aisle.ItemPrefab.name]++;
                 aisle = null;
             }
             Debug.Log("[SHOPPING LIST] NEW LIST IS");
             foreach (var item in CurrentList)
             {
-                Debug.Log($"{item.Key.name} x {item.Value}");
+                Debug.Log($"{item.Key} x {item.Value}");
             }
         }
     }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -38,6 +38,8 @@ namespace GJgame
         public LabelLibrary LabelLibrary;
 
         public int Difficulty = 0;
+
+        public EndZoneTrigger TriggerForEnd;
         private void Start()
         {
             ItemLibrary.Init();
@@ -80,11 +82,78 @@ namespace GJgame
             PlayerCamera.LookAt = Player.transform;
             Jay.SetAiActive(true);
             BuyListManager.Instance.GenerateList(Difficulty * 2);
+            TriggerForEnd.gameObject.SetActive(true);
         }
 
         public AisleConstructor GetRandomAisle()
         {
             return Aisles[Random.Range(0, Aisles.Length)];
+        }
+
+        public void TriggerLevelEnd()
+        {
+            if(Player.CurrentPickup is CartMovement)
+            {
+                BeginVictoryCheck();
+            }
+            else
+            {
+                Debug.Log($"BRING THE CART TO END LEVEL");
+            }
+        }
+
+        public void BeginVictoryCheck()
+        {
+            TriggerForEnd.gameObject.SetActive(false);
+            var currentItems = FindObjectOfType<CartBasket>().CartInventory;
+            var neededItems = BuyListManager.Instance.CurrentList;
+            while(currentItems.Count > 0)
+            {
+                var item = currentItems.Pop();
+                if (neededItems.TryGetValue(item.name, out var value))
+                {
+                    if (value <= 0)
+                    {
+                        GameOver("MORE ITEMS THAN NEEDED GAME OVER");
+                        return;
+                    }
+                    neededItems[item.name]--;
+                }
+                else
+                {
+                    GameOver("ITEM NOT IN THE LIST. GAME OVER");                  
+                    return;
+                }
+            }
+            foreach (var item in neededItems)
+            {
+                if(item.Value > 0)
+                {
+                    GameOver("NOT ENOUGH ITEMS. GAME OVER");
+                    return;
+                }
+            }
+
+            Win("YOU WON");
+        }
+
+        private void GameOver(string text)
+        {
+            Debug.Log(text);
+            Difficulty = 0;
+            StartCoroutine(RestartIn(5));
+        }
+
+        private void Win(string text)
+        {
+            Debug.Log(text);
+            StartCoroutine(RestartIn(5));
+        }
+
+        private IEnumerator RestartIn(float time)
+        {
+            yield return new WaitForSeconds(time);
+            Restart();
         }
     }
 }
